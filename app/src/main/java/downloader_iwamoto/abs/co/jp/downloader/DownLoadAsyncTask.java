41ba2fe6b7b6c9d129f53f61074f7c6516387723
chihirons <chihi60323@icloud.com>
2018-05-23 16:55:52 +0900

    ダウンローダーVer.1

---
+++ app/src/main/java/downloader_iwamoto/abs/co/jp/downloader/DownLoadAsyncTask.java
@@ -0,0 +1,105 @@
+package downloader_iwamoto.abs.co.jp.downloader;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+public class DownLoadAsyncTask extends AsyncTask<String, Void, Bitmap> {
+
+    private Listener listener;
+
+    //非同期処理
+    @Override
+    protected Bitmap doInBackground(String... strings) {
+        return downloadImage(strings[0]);
+    }
+
+    //途中経過をメインスレッドに返す
+    @Override
+    protected void onProgressUpdate(Void... voids){
+        //working cursorを表示させるとよい
+    }
+
+    //非同期処理が終了後、結果をメインスレッドに返す
+    @Override
+    protected void onPostExecute(Bitmap bitmap){
+        if (listener != null) listener.onSuccess(bitmap);
+    }
+
+    //イメージ取得用の処理
+    private Bitmap downloadImage(String address){
+        Bitmap bmp = null;
+
+        final StringBuilder result = new StringBuilder();
+        //初期化
+        HttpURLConnection urlConnection = null;
+
+        try{
+            URL url = new URL(address);
+            //HttpURLConnectionインスタンス生成
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            //タイムアウト設定
+            urlConnection.setReadTimeout(1000);
+            urlConnection.setConnectTimeout(2000);
+
+            //リクエストメソッド
+            urlConnection.setRequestMethod("GET");
+
+            //リダイレクトを自動で許可しない
+            urlConnection.setInstanceFollowRedirects(false);
+
+            //ヘッダー設定
+            //urlConnection.setRequestProperty();
+
+            //接続
+            urlConnection.connect();
+            int resp = urlConnection.getResponseCode();
+
+            switch(resp){
+                case HttpURLConnection.HTTP_OK:
+                    InputStream is = null;
+                    try {
+                        is = urlConnection.getInputStream();
+                        bmp = BitmapFactory.decodeStream(is);
+                        is.close();
+                    }catch (IOException e){
+                        e.printStackTrace();
+                    }finally {
+                        if(is != null){
+                            is.close();
+                        }
+                    }
+
+                    break;
+                case HttpURLConnection.HTTP_UNAUTHORIZED:
+                    break;
+                default:
+                    break;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            if (urlConnection != null){
+                urlConnection.disconnect();
+            }
+        }
+        return bmp;
+    }
+
+    void setListener(Listener listener){
+        this.listener = listener;
+    }
+
+    interface Listener{
+        void onSuccess(Bitmap bmp);
+    }
+}
--- app/src/main/java/downloader_iwamoto/abs/co/jp/downloader/MainActivity.java
+++ app/src/main/java/downloader_iwamoto/abs/co/jp/downloader/MainActivity.java
@@ -1,13 +1,75 @@
 package downloader_iwamoto.abs.co.jp.downloader;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText Text;
+    Button Start;
+    ImageView View;
+    private DownLoadAsyncTask Task;
+
+    Toast No;
+    Toast Ok;
+    String Url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        No = Toast.makeText(this, "画像取得に失敗",Toast.LENGTH_SHORT);
+        Ok = Toast.makeText(this,"ダウンロードが完了しました",Toast.LENGTH_SHORT);
+
+        //紐づけ
+        Text = findViewById(R.id.text);
+        Start = findViewById(R.id.start);
+        View = findViewById(R.id.view);
+
+        //ダウンロード開始ボタンタップ時
+        Start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Url = Text.getText().toString();
+
+                if(Url.length() != 0){
+                    //EditTextの文字が入っていた場合
+                    Task = new DownLoadAsyncTask();
+                    //Lisntenerを設定
+                    Task.setListener(createListener());
+                    Task.execute(Url);
+                }else{
+                    No.show();
+                }
+            }
+        });
     }
+
+    @Override
+    protected void onDestroy() {
+        Task.setListener(null);
+        super.onDestroy();
+    }
+
+    private DownLoadAsyncTask.Listener createListener(){
+        return new DownLoadAsyncTask.Listener() {
+            @Override
+            public void onSuccess(Bitmap bmp) {
+                View.setImageBitmap(bmp);
+                if (bmp == null) {
+                    No.show();
+                }else{
+                    Ok.show();
+                }
+            }
+        };
+    }
+
 }
